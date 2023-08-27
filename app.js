3b20df1a5396056cba4accf878447287e3f521cb
durgaprasadsiddula <sdurgaprasad4723@gmail.com>
2023-08-28 00:10:26 +0530

    deleteing booking

--- app.js
+++ app.js
@@ -1,48 +1,4 @@
-// require("dotenv").config();
-// const cors = require('cors');
-// const exp=require("express");
-// const app=exp();
 
-// app.use(exp.json())
-// const PORT = process.env.PORT || 4000
-// app.use(cors());
-// let mclient=require("mongodb").MongoClient;
-
-// let DBurl=process.env.DBurl;
-
-// mclient.connect(DBurl)
-// .then((client)=>{
-//     let dbobj=client.db("oneshot");
-//     let userCollectionObj=dbobj.collection("users");
-//     app.set("userCollectionObj",userCollectionObj);
-//     console.log("DB connection success");
-// })
-// .catch((error)=>{
-//     console.log("Error in DB connection");
-// })
-
-
-
-
-
-// app.get("/get-user",async(request,response)=>{
-//     let userCollectionObj=app.get("userCollectionObj");
-//     let users=await userCollectionObj.find().toArray();
-//     response.send({message:"All users",payload:users});
-// })
-
-// app.post("/post-user",async(request,response)=>{
-//     let obj=request.body;
-//     console.log(obj);
-//     let userCollectionObj=app.get("userCollectionObj");
-//     await userCollectionObj.insertOne(obj);
-//     response.send({message:"user created successfully"})
-// })
-
-
-// app.listen(PORT,()=>{
-//     console.log("Server listening on port 4000");
-// })
 
 
 require("dotenv").config();
@@ -125,12 +81,14 @@ app.delete("/delete-booking/:id", async (request, response) => {
   let id = request.params.id; // Use request.params.id to get the ID from the URL parameter
   console.log(id);
   try {
-    let bookings = await bookingCollectionObj.deleteOne({ "_id":id }); // Make sure to use ObjectId to convert the ID to MongoDB ObjectId
+    let bookings = await bookingCollectionObj.deleteOne({ "_id": new ObjectId(id) }); // Use new ObjectId to convert the ID to MongoDB ObjectId
     response.send({ message: "Booking deleted" });
   } catch (error) {
     response.status(500).send({ message: "Error deleting slot", error: error.message });
   }
 });
+
+
 
 
 app.listen(PORT, () => {
